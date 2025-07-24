05f6686847da07af66def355660a35017c164243
Atay Özdemir <atayozdemir01@gmail.com>
2025-07-24 21:45:24 +0300

    ilk aşama tmmgibi

--- building/main.cpp
+++ building/main.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 
 int main(){ //1 8 16 24 32
-    int a = 0;
-    int sehirbuyuklugu = 4;
-    for(int b=0 ; b < sehirbuyuklugu ; b++){
-        a = a + 8;
+    int x = 0;
+    int y = 0;
+    int sehirbuyuklugux = 0;
+    int sehirbuyukluguy = 0;
+    for(int i = 0 ; i < sehirbuyuklugux ; i++){
+        x = x + 8;
+    }
+    for(int i = 0 ; i < sehirbuyukluguy ; i++){
+        y = y + 8;
     }
     int flag = 0;
-    do{
-        for(int i = -1 ; i<a ; i++){
-            cout << "a";
-        }
+    if(x==0)
+        x++;
+    if(y==0)
+        y++;
+    
         flag++;
-    }while(flag != sehirbuyuklugu);
+        for(int i = 0 ; i < y ; i++){
+            for(int j = 0 ; j < x ; j++){
+                cout << "a ";
+            }
+            cout << endl;
+        }
 }
 
